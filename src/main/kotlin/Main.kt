38f6aaca72902153764f968204b8f0e1f2514c2c
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-16 22:09:47 +0200

    implement main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,38 @@
-package org.example
+import data.CsvFood
+import logic.FoodService
+import java.time.LocalDate
 
 fun main() {
-    println("Hello World!")
+    val csvPath = "food.csv"
+    val repository = CsvFood(csvPath)
+    val service = FoodService(repository)
+
+    print("Enter date (YYYY-MM-DD): ")
+    val inputDate = readLine()
+
+    try {
+        val date = LocalDate.parse(inputDate)
+        val meals = service.getMealsByDate(date)
+
+        if (meals.isEmpty()) {
+            println("No meals found.")
+            return
+        }
+
+        meals.forEach { println("ID: ${it.id}, Name: ${it.name}") }
+
+        print("\nEnter ID to view description: ")
+        val id = readLine()?.toIntOrNull()
+
+        val desc = id?.let { service.getMealDescriptionById(meals, it) }
+
+        if (desc != null) {
+            println("\nDescription: $desc")
+        } else {
+            println("\nInvalid ID or meal not found.")
+        }
+
+    } catch (e: Exception) {
+        println("Invalid input. Error: ${e.message}")
+    }
 }
