a6b246c84c7d7a13d6fcf98198c117c8936786d5
AimanYosofi <=>
2025-04-17 22:51:04 +0200

    initial packages of dependency injection and console ui

---
+++ src/main/kotlin/dependencyInjection/appModule.kt
@@ -0,0 +1,9 @@
+package org.example.dependencyInjection
+
+import org.example.data.MockDataRepository
+import org.example.logic.MealRepository
+import org.koin.dsl.module
+
+val appModule = module {
+    single<MealRepository> { MockDataRepository() }
+}
