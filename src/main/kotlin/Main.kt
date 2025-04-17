4c5575a6faae7679b358d7ec2a5b662b4c911a26
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-04-17 20:18:32 +0300

    push Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
 package org.example
 
+import org.example.logic.KetoDietMealHelper
+import org.example.utils.CsvParserImpl
+import java.util.Scanner
+
 fun main() {
-    println("Hello World!")
+    val scanner = Scanner(System.`in`)
+    val parser = CsvParserImpl()
+    val helper = KetoDietMealHelper(parser)
+
+    helper.loadMeals("src/main/kotlin/food.csv")
+
+    while (true) {
+        val mealData = helper.getKetoMeal()
+            println("Suggested meal: ${mealData["Name"]}")
+            println("Description: ${mealData["Description"]}")
+            println("Do you like it? (yes/no)")
+
+        val response = scanner.nextLine().trim().lowercase()
+
+        if (response == "yes") {
+            println("\nYou liked it! Here's the full meal:")
+            println("Name: ${mealData["Name"]}")
+            println("Description: ${mealData["Description"]}")
+            println("Fat (g): ${mealData["Fat (g)"]}")
+            println("Protein (g): ${mealData["Protein (g)"]}")
+            println("Carbs (g): ${mealData["Carbs (g)"]}")
+            break
+        } else {
+            println("Okay, finding another meal...\n")
+        }
+    }
 }
