b26a3dfb66a03f7d3fa0d66b55cb3463d6f64e66
AimanYosofi <=>
2025-04-17 20:53:10 +0200

    prepare mock-data repository

---
+++ src/main/kotlin/model/Meal.kt
@@ -0,0 +1,16 @@
+package org.example.model
+
+data class Meal(
+    val id: Int,
+    val name: String,
+    val minutes: Int,
+    val contributorId: Int,
+    val submitted: Int,
+    val tags: List<String>,
+    val nutrition: Nutrition,
+    val nSteps: Int,
+    val steps: List<String>,
+    val description: String?,
+    val ingredients: List<String>,
+    val nIngredients: Int,
+)
