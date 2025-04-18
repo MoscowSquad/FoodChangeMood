f280be15ba1a019664c658c873f5375379095ce7
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-04-19 01:17:38 +0300

    add use case module for task 7

--- src/main/kotlin/logic/KetoDietMealHelper.kt
+++ src/main/kotlin/logic/KetoDietMealHelper.kt
@@ -13,22 +13,22 @@ class KetoDietMealHelper(private val repository: MealRepository) {
     }
 
     // Check if the meal is keto-friendly based on the nutritional info
-    private fun isKetoFriendly(nutrition: Nutrition): Boolean {
-        return nutrition.carbohydrates < 10 && (nutrition.totalFat + nutrition.saturatedFat) > nutrition.protein
+    private fun isKetoFriendly(nutrition: Nutrition?): Boolean {
+        return (nutrition?.carbohydrates!!) < 10 && (nutrition?.totalFat!! > nutrition.protein!!)
     }
 
     // Suggest a keto meal
     fun getKetoMeal(): Map<String,Any> {
         for (meal in ketoMeals) {
             if (meal.name !in suggestedMeals) {
-                suggestedMeals.add(meal.name)
+                suggestedMeals.add(meal.name!!)
                 return mapOf(
                     "Name" to (meal.name ?: "No Name"),
                     "Description" to (meal.description ?: "No description"),
-                    "TotalFat (g)" to (meal.nutrition.totalFat ?: "No TotalFat (g)"),
-                    "Protein (g)" to (meal.nutrition.protein ?: "No Protein (g)"),
-                    "SaturatedFat (g)" to (meal.nutrition.saturatedFat ?: "No TSaturatedFat (g)"),
-                    "Carbohydrates (g)" to (meal.nutrition.carbohydrates ?: "No Carbohydrates (g)")
+                    "TotalFat (g)" to (meal.nutrition?.totalFat ?: "No TotalFat (g)"),
+                    "Protein (g)" to (meal.nutrition?.protein ?: "No Protein (g)"),
+                    //"SaturatedFat (g)" to (meal.nutrition.saturatedFat ?: "No TSaturatedFat (g)"),
+                    "Carbohydrates (g)" to (meal.nutrition?.carbohydrates ?: "No Carbohydrates (g)")
                 )
             }
         }
