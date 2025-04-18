4f141ffafe80888b385e801a991593495f678f21
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-18 18:05:10 +0200

    change type of submitted

--- src/main/kotlin/model/Meal.kt
+++ src/main/kotlin/model/Meal.kt
@@ -1,11 +1,13 @@
 package org.example.model
 
+import java.util.Date
+
 data class Meal(
     val id: Int,
     val name: String,
     val minutes: Int,
     val contributorId: Int,
-    val submitted: Int,
+    val submitted: Date,
     val tags: List<String>,
     val nutrition: Nutrition,
     val nSteps: Int,
