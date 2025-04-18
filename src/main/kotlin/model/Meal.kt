12b2bea9e152ef895fd94d0ad63d34ee70dbefd7
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-18 18:25:31 +0200

    change submitted type from date to int

--- src/main/kotlin/model/Meal.kt
+++ src/main/kotlin/model/Meal.kt
@@ -7,7 +7,7 @@ data class Meal(
     val name: String,
     val minutes: Int,
     val contributorId: Int,
-    val submitted: Date,
+    val submitted: Int,
     val tags: List<String>,
     val nutrition: Nutrition,
     val nSteps: Int,
