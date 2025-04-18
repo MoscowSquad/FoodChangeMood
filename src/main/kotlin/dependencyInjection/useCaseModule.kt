a527fbfa50f05a3b351542315f16a672891ec023
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-18 19:59:22 +0200

    add random meals contains potatoes to the di

--- src/main/kotlin/dependencyInjection/useCaseModule.kt
+++ src/main/kotlin/dependencyInjection/useCaseModule.kt
@@ -1,10 +1,12 @@
 package org.example.dependencyInjection
 
+import org.example.logic.GetRandomMealsHavePotatoes
 import org.example.logic.SearchMealByNameUseCase
 import org.example.usecases.SweetsWithNoEggUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { SearchMealByNameUseCase(get(), get()) }
+    single { GetRandomMealsHavePotatoes(get()) }
     single { SweetsWithNoEggUseCase(get()) }
 }
