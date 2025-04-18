fe0f03c8ede05e2477a7f6b60ccba4d1891fbd58
Hassan Wasfy <hassanwasfy7@gmail.com>
2025-04-18 18:22:45 +0200

    implement a custom parser test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package org.example
 
+import org.example.utils.CustomParser
+
 fun main() {
     println("Hello World!")
+}
+
+
+fun mainParsingTest(){
+    val parser = CustomParser()
+    val file = parser.getResourceFile("food.csv")
+    val list = parser.parseMealsCsv(file)
+    list.take(10).forEach { item ->
+        println(item.toString())
+    }
 }
