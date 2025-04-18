c1f9ba824f9a29ea4292d03d98d7062a34152a8c
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-04-18 19:49:52 +0200

    refactor code and handle some issues

--- src/main/kotlin/logic/FoodRepo.kt
+++
@@ -1,8 +0,0 @@
-package org.example.logic
-
-import model.Food
-
-
-interface FoodRepo {
-    fun getAllFood():List<Food>
-}
--- src/main/kotlin/logic/IngredientGame.kt
+++ src/main/kotlin/logic/IngredientGame.kt
@@ -1,6 +1,6 @@
 package org.example.logic
 
-import model.Food
+import org.example.model.Meal
 
 /*
 11- Ingredient Game: Display a meal name and three ingredient options
@@ -10,54 +10,66 @@ The user guesses once. A correct guess earns 1000 points;
  The game also ends after 15 correct answers.
   Display the final score at the end.
  */
-class IngredientGame(val foodRepo: FoodRepo) {
+class IngredientGame(private val mealrepo: MealRepository) {
     fun ingredientGame(){
         var points: Int = 0
         var firstIndex=0
         var lastIndex=2
         var continueAsking = false
         val  questionsNumber=15
-        var currentQuestionNumber = 0
+        var currentQuestionNumber = 1
 
-        val allMeals = foodRepo.getAllFood()
-        val gameMealList= mutableListOf<Food>()
-        while (currentQuestionNumber<questionsNumber) {
-            println("-- Welcome to ingredient game --")
-            println(
-                "note: \"This game will show you meal name,\n" +
-                        " you will have 3 ingredient options to choose between\n" +
-                        " just one of them is correct\""
-            )
-            println("*****************************************************************")
-            allMeals.filter { it.ingredients != null }
+        val allMeals = mealrepo.getAllMeals()
+        val gameMealList= mutableListOf<Meal>()
+        val optionsList= mutableListOf<List<String>>()
+        println("-- Welcome to ingredient game --")
+        println(
+            "note: \"This game will show you meal name,\n" +
+                    " you will have 3 ingredient options to choose between\n" +
+                    " just one of them is correct\""
+        )
+        println("*****************************************************************")
+        while (currentQuestionNumber<=questionsNumber) {
+
+
+            allMeals
                 .shuffled()
                 .take(3)
                 .forEach { gameMealList.add(it) }
             for (index in firstIndex..lastIndex) {
-                continueAsking = showMealAndOptions(
-                    mealName = gameMealList[firstIndex].mealName,
-                    correctOption = gameMealList[firstIndex].ingredients!!,
-                    optionsList = listOf(gameMealList[index].ingredients!!)
-                )
+                optionsList.add(gameMealList[index].ingredients)
             }
+            continueAsking = showMealAndOptions(
+                mealName = gameMealList[firstIndex].name,
+                correctOption = gameMealList[firstIndex].ingredients,
+                optionsList = optionsList,
+                currentQuestionNumber = currentQuestionNumber,
+
+            )
             if (continueAsking) {
                 points += 1000
                 currentQuestionNumber++
+                println("Great!!!")
+                println("+1000 point")
+                println("you've got $points point")
+
             }else{
-                break
+                println("Wrong answer!!")
+                println("you've got $points point")
+                return
             }
         }
 
     }
 
-    private fun showMealAndOptions(mealName:String, correctOption:String, optionsList:List<String>):Boolean{
+    private fun showMealAndOptions(currentQuestionNumber: Int,mealName:String, correctOption:List<String>, optionsList:List<List<String>>):Boolean{
         val options=optionsList.shuffled()
         println(mealName)
         println("------------------------------")
-        for (index in 0 .. 2) println("$index-${options[index]}")
+        for (index in 0 .. 2) println("${index+1}-${options[index]}")
 
-        print("choose answer: ")
+        print("Q$currentQuestionNumber: choose answer: ")
         val input= readln()
-        return options[input.toInt()]==correctOption
+        return options[input.toInt()-1]==correctOption
     }
 }
