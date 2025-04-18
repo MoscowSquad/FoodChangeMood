e64690623e75706062e352fefce51d70cc45e48f
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-18 18:50:30 +0200

    fix sweet with no eggs use case

--- src/main/kotlin/usecases/SweetsWithNoEggUseCase.kt
+++ src/main/kotlin/usecases/SweetsWithNoEggUseCase.kt
@@ -5,7 +5,7 @@ import org.example.model.Meal
 class SweetsWithNoEggUseCase(private val meals: List<Meal>) {
     operator fun invoke(): Meal? {
         return meals
-            .filter { it.tags.contains("desserts") && !it.ingredients.contains("egg") }
+            .filter { it.tags.contains("desserts") && !it.ingredients.contains("eggs") }
             .randomOrNull()
     }
 }
