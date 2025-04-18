1a1366f9dc4f8ba31f6ad9aefe0c33973cc28e47
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-18 18:28:27 +0200

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,22 @@
 package org.example
 
+
+import org.example.dependencyInjection.appModule
+import org.example.presentation.FoodChangeMoodConsoleUI
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.inject
+import org.example.logic.MealRepository
+
+
+
 fun main() {
+    startKoin {
+        modules(appModule)
+    }
+
+    val mealRepository: MealRepository by inject(MealRepository::class.java)
+    val ui = FoodChangeMoodConsoleUI(mealRepository)
+    ui.searchMealsByDate()
+
+
 }
