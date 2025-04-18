df6dea89a8b1fa7b6435cd79b4d8bbd963c9df2b
Mohammed ALjabouri <eng.moha100@gmail.com>
2025-04-18 18:44:09 +0300

    add edit formating in display

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ fun main() {
 
     val mock= MockDataRepository()
      mock.getIraqiMeals().forEach {
-         println("\n\n Display Iraqi Menu Meals Please :)")
+         println("Display Iraqi Menu Meals Please : ")
 
          println("the common meals in iraq is ${it.name}")
      }
