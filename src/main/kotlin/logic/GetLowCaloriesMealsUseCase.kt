9464590f6aa4b83d72075fe576c14dcdfb9e5c53
AimanYosofi <=>
2025-04-18 22:50:21 +0200

    add get-high-calories-meals use case

---
+++ src/main/kotlin/logic/GetLowCaloriesMealsUseCase.kt
@@ -0,0 +1,26 @@
+package org.example.logic
+
+import org.example.exceptions.Exceptions
+import org.example.model.Meal
+
+const val HIGH_CALORIE_THRESHOLD = 700
+
+class GetHighCaloriesMealsUseCase(private val repository: MealRepository) {
+    private val suggestedList = mutableSetOf<Meal>()
+    fun invoke(): Meal {
+        repository.getAllMeals()
+            .filter(::byHighCalories)
+            .forEach { meal ->
+                if (suggestedList.contains(meal)) {
+                    suggestedList.add(meal)
+                    return meal
+                }
+            }
+
+        throw Exceptions.MealNotFoundException()
+    }
+
+    private fun byHighCalories(meal: Meal): Boolean {
+        return meal.nutrition.calories > HIGH_CALORIE_THRESHOLD
+    }
+}
