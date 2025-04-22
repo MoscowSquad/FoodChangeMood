d8e73fced0bf65b91b60538b334088955adc03a4
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-22 15:57:47 +0200

    refactor searchMealsByCountry add exception

--- src/main/kotlin/logic/usecases/SearchMealByCountryUseCase.kt
+++ src/main/kotlin/logic/usecases/SearchMealByCountryUseCase.kt
@@ -1,6 +1,7 @@
 package org.example.logic.usecases
 
 import org.example.logic.repository.MealRepository
+import org.example.model.Exceptions
 import org.example.model.Meal
 
 class SearchMealByCountryUseCase(
@@ -9,8 +10,10 @@ class SearchMealByCountryUseCase(
     fun searchMealsByCountry(country: String): List<Meal> {
         return repository.getAllMeals()
             .filter { meal -> meal.matchesCountry(country) }
-            .shuffled()
-            .take(MAX_MEALS)
+            .takeIf { it.isNotEmpty() }
+            ?.shuffled()
+            ?.take(MAX_MEALS)
+            ?: throw Exceptions.NoMealsFound("No Meal Found With Country: $country")
     }
 
     private fun Meal.matchesCountry(country: String): Boolean{
@@ -20,11 +23,10 @@ class SearchMealByCountryUseCase(
                 description?.contains(lowerCountry , ignoreCase = true) == true
     }
 
+
     companion object {
         const val MAX_MEALS = 20
     }
 }
 
-// 1. Extract the logic inside the filter to another function
 // 2. Use random indices instead of shuffled
-// 3. Use the exception you have implemented if the list is empty
