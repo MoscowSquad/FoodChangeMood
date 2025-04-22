44eea95955d371e57c66f19533fac51162b5d4e4
= <=>
2025-04-22 22:46:13 +0300

    refactor into functional programming

--- src/main/kotlin/logic/usecases/RandomMealNameProviderUseCase.kt
+++ src/main/kotlin/logic/usecases/RandomMealNameProviderUseCase.kt
@@ -3,19 +3,14 @@ package org.example.logic.usecases
 import org.example.logic.repository.MealRepository
 import org.example.model.Exceptions
 import org.example.model.Meal
+
 class RandomMealNameProviderUseCase(
     private val mealRepository: MealRepository
 ) {
-    fun getRandomMeal(): Meal {
-        val meals = mealRepository.getAllMeals()
+    private fun isValidMeal(meal: Meal): Boolean = !meal.name.isNullOrBlank()
 
-        if (meals.isEmpty()) {
-            throw Exceptions.IncorrectMealNameException()
-        }
-
-        return meals
-            .filter { !it.name.isNullOrBlank() }
-            .randomOrNull()
-            ?: throw Exceptions.NoFoodFoundException()
-    }
+    fun getRandomMeal(): Meal = mealRepository.getAllMeals()
+        .filter(::isValidMeal)
+        .randomOrNull()
+        ?: throw Exceptions.NoFoodFoundException()
 }
