40013c387051081c731b65966132ba18b1dda325
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-22 14:12:59 +0200

    refactor getMealById

--- src/main/kotlin/logic/usecases/GetMealByIdUseCase.kt
+++ src/main/kotlin/logic/usecases/GetMealByIdUseCase.kt
@@ -1,16 +1,17 @@
 package org.example.logic.usecases
 
 import org.example.logic.repository.MealRepository
+import org.example.model.Exceptions
 import org.example.model.Meal
 
 class GetMealByIdUseCase(
     private val repository: MealRepository
 ) {
-    fun getMealById(id: Int): Meal? {
+    fun getMealById(id: Int): Meal {
         return repository.getAllMeals().find { it.id == id }
+            ?: throw Exceptions.NoMealsFound("No Meal Found with ID: $id")
     }
 }
 
 
 // 3. Use the exception you have implemented if the list is empty
-// 4. Fix the function as you want it to function
