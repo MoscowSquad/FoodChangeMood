3f9edda63cf1cd0a7d333995f10b829684a8ad7e
alaafawzy <alaaf9443@gmail.com>
2025-04-23 15:39:44 +0200

    change invoke function logic to make it get meal and filter it

--- src/main/kotlin/logic/usecases/FindItalianMealsForLargeGroupsUseCase.kt
+++ src/main/kotlin/logic/usecases/FindItalianMealsForLargeGroupsUseCase.kt
@@ -1,48 +1,20 @@
 package org.example.logic.usecases
 
 import org.example.logic.repository.MealRepository
+import org.example.model.Exceptions
 import org.example.model.Meal
 
 class FindItalianMealsForLargeGroupsUseCase(
     private val mealRepository: MealRepository
 ) {
     operator fun invoke(): List<Meal> {
-        val allMeals = getAllMeals()
-        val filteredMeals = filterItalianAndGroupSuitableMeals(allMeals)
-        return filteredMeals
-    }
+        val meals = mealRepository.getAllMeals()
+        //.filter(::isItalianMealAndGroupSuitableMeal)
+        if (meals.isEmpty()) throw Exceptions.NoMealsFoundException()
+        return meals
 
-    private fun getAllMeals(): List<Meal> {
-        return mealRepository.getAllMeals()
-    }
-
-    private fun filterItalianAndGroupSuitableMeals(meals: List<Meal>): List<Meal> {
-        return meals.filter { meal ->
-            isItalianMeal(meal) && isGroupSuitableMeal(meal)
-        }
-    }
-
-    private fun isItalianMeal(meal: Meal): Boolean {
-        val tagsLower = meal.tags?.map { it.lowercase() }
-        if (tagsLower != null) {
-            return tagsLower.contains("italian") || meal.description?.lowercase()?.contains("italian") ?: false
-        }
-        return false
-    }
-
-    private fun isGroupSuitableMeal(meal: Meal): Boolean {
-        val tagsLower = meal.tags?.map { it.lowercase() }
-        if (tagsLower != null) {
-            return tagsLower.contains("for-large-groups") ||
-                    tagsLower.contains("dinner-party") ||
-                    tagsLower.contains("potluck")
-        }
-        return false
     }
 }
 
 
-// 1. Add function to get high quality data
-// 2. Merge isItalianMeal and isGroupSuitableMeal into one function and use filter(::theNameOfTheNewFunction)
-// 3. Reduce the logic into the invoke function by reducing the number of called functions
-// 4. Check if the list is empty or not if it is throw a custom exception and write it on the Exceptions file
+
