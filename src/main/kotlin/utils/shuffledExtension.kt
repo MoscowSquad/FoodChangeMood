740995a8f6c824592742cb62e8835b841a4633b2
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-23 11:18:58 +0200

    fix takeRandomMeals function issue

--- src/main/kotlin/utils/shuffledExtension.kt
+++ src/main/kotlin/utils/shuffledExtension.kt
@@ -1,8 +1,11 @@
 package org.example.utils
 
+import kotlin.random.Random
+
 fun <T> List<T>.takeRandomMeals(numberOfMeals: Int): List<T> {
-    return generateSequence { this.random() }
+    return generateSequence { Random.nextInt(numberOfMeals) }
         .distinct()
-        .take(numberOfMeals)
+        .take(numberOfMeals).also { println(it) }
         .toList()
+        .map(this::get)
 }
