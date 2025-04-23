ba5a8da5da96a4c315741ba0afb43308c5b2e83d
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-23 15:27:29 +0200

    refactor takeRandomMeals

--- src/main/kotlin/utils/shuffledExtension.kt
+++ src/main/kotlin/utils/shuffledExtension.kt
@@ -1,8 +1,18 @@
 package org.example.utils
 
+//fun <T> List<T>.takeRandomMeals(numberOfMeals: Int): List<T> {
+//    return generateSequence { this.random() }
+//        .distinct()
+//        .take(numberOfMeals)
+//        .toList()
+//}
 fun <T> List<T>.takeRandomMeals(numberOfMeals: Int): List<T> {
-    return generateSequence { this.random() }
-        .distinct()
-        .take(numberOfMeals)
-        .toList()
+    return toSet()
+        .let { uniqueItems ->
+            if (uniqueItems.size <= numberOfMeals) uniqueItems.toList()
+            else generateSequence { uniqueItems.random() }
+                .distinct()
+                .take(numberOfMeals)
+                .toList()
+        }
 }
