e76f2243c8f5f790bde6f1b8fb0d69588820935f
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-23 15:26:45 +0200

    ignore unused

--- src/main/kotlin/presentation/GuessGameUI.kt
+++ src/main/kotlin/presentation/GuessGameUI.kt
@@ -1,32 +1,32 @@
-package org.example.presentation
-
-import org.example.logic.usecases.RandomMealNameProvider
-import org.example.model.Meal
-
-class GuessGameUI(
-    private val randomMealNameUseCase: RandomMealNameProvider
-) {
-    operator fun invoke() {
-        println("Prepare a meal to guess by you ...")
-        val meal = randomMealNameUseCase.getRandomMeal()
-        println("Guess the preparation time for (${meal.name}): ")
-        guessGame(meal)
-    }
-
-    private fun guessGame(meal: Meal, time: Int = 0) {
-        if (time == 3) {
-            println("later")
-            return
-        }
-
-        print("Preparation time:")
-        val suggestion = readln()
-        if (meal.minutes == suggestion.toIntOrNull()) {
-            println("You are correct")
-            return
-        } else {
-            print("Not correct. Guess again, ")
-            guessGame(meal, time + 1)
-        }
-    }
-}
+//package org.example.presentation
+//
+//import org.example.logic.usecases.RandomMealNameProvider
+//import org.example.model.Meal
+//
+//class GuessGameUI(
+//    private val randomMealNameUseCase: RandomMealNameProvider
+//) {
+//    operator fun invoke() {
+//        println("Prepare a meal to guess by you ...")
+//        val meal = randomMealNameUseCase.getRandomMeal()
+//        println("Guess the preparation time for (${meal.name}): ")
+//        guessGame(meal)
+//    }
+//
+//    private fun guessGame(meal: Meal, time: Int = 0) {
+//        if (time == 3) {
+//            println("later")
+//            return
+//        }
+//
+//        print("Preparation time:")
+//        val suggestion = readln()
+//        if (meal.minutes == suggestion.toIntOrNull()) {
+//            println("You are correct")
+//            return
+//        } else {
+//            print("Not correct. Guess again, ")
+//            guessGame(meal, time + 1)
+//        }
+//    }
+//}
