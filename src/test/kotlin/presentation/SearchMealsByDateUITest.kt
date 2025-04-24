83a63305ed762aab20313ddd013483687f536794
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-24 22:05:08 +0200

    refactor SearchMealsByDateUITest

--- src/test/kotlin/presentation/SearchMealsByDateUITest.kt
+++ src/test/kotlin/presentation/SearchMealsByDateUITest.kt
@@ -1,20 +1,26 @@
 package presentation
 
 import io.mockk.mockk
+import org.example.logic.usecases.GetMealByIdUseCase
 import org.example.logic.usecases.GetMealsByDateUseCase
 import org.example.presentation.SearchMealsByDateUI
 import org.junit.jupiter.api.BeforeEach
+import java.util.*
 
 class SearchMealsByDateUITest {
     private lateinit var getMealsByDateUseCase: GetMealsByDateUseCase
+    private lateinit var getMealByIdUseCase: GetMealByIdUseCase
     private lateinit var consoleIO: FakeConsoleIO
     private lateinit var searchMealsByDateUI: SearchMealsByDateUI
 
     @BeforeEach
     fun setup() {
-        getMealsByDateUseCase = mockk(relaxed = true)
-        consoleIO = mockk(relaxed = true)
-        searchMealsByDateUI = mockk(relaxed = true)
+        getMealsByDateUseCase = mockk()
+        getMealByIdUseCase = mockk()
+        consoleIO = FakeConsoleIO(LinkedList())
+        searchMealsByDateUI = SearchMealsByDateUI(getMealsByDateUseCase, getMealByIdUseCase, consoleIO)
+
     }
+
 
 }
