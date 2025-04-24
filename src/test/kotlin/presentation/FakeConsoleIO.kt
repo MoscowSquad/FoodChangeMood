c8c51f1b0541d27b8e43459ed413aeef8e48bdd7
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-24 14:54:43 +0200

    fix fake console io

--- src/test/kotlin/presentation/FakeConsoleIO.kt
+++ src/test/kotlin/presentation/FakeConsoleIO.kt
@@ -5,10 +5,10 @@ import java.util.*
 
 class FakeConsoleIO(
     private val inputs: Queue<String>,
-    val outputs: MutableList<String> = mutableListOf()
+    val outputs: MutableList<String?> = mutableListOf()
 ) : ConsoleIO {
-    override fun read(): String? = inputs.poll()
-    override fun write(message: String) {
+    override fun read(): String = inputs.poll()
+    override fun write(message: String?) {
         outputs.add(message)
     }
 }
