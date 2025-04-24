c9de8842399ab21fb7230841ddddf84f2cff60d1
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-24 22:06:49 +0200

    add should return meals matching with date test

--- src/test/kotlin/presentation/SearchMealsByDateUITest.kt
+++ src/test/kotlin/presentation/SearchMealsByDateUITest.kt
@@ -1,10 +1,14 @@
 package presentation
 
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
+import logic.usecases.createMeal
 import org.example.logic.usecases.GetMealByIdUseCase
 import org.example.logic.usecases.GetMealsByDateUseCase
 import org.example.presentation.SearchMealsByDateUI
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import java.util.*
 
 class SearchMealsByDateUITest {
@@ -22,5 +26,24 @@ class SearchMealsByDateUITest {
 
     }
 
+    @Test
+    fun `should return meals matching with date`() {
+        // Given
 
+        val dateInput = "2006-05-04"
+        val mealIdInput = "12345"
+        consoleIO.inputs.add(dateInput)
+        consoleIO.inputs.add(mealIdInput)
+
+        val trueMeal = createMeal(id = 12345, name = "Burrito", submitted = "2006-05-04")
+        every { getMealsByDateUseCase.getMealsByDate(dateInput) } returns listOf(trueMeal)
+        every { getMealByIdUseCase.getMealById(12345) } returns trueMeal
+
+        // When
+        searchMealsByDateUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("\nMeals found:")
+        assertThat(consoleIO.outputs).contains("ID: 12345, Name: Burrito")
+    }
 }
