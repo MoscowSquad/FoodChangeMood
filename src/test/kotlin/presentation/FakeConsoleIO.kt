b74d7afd4b9c9a3b46340b90b56dac6885823524
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-24 14:53:08 +0200

    add fake console io

---
+++ src/test/kotlin/presentation/FakeConsoleIO.kt
@@ -0,0 +1,14 @@
+package presentation
+
+import org.example.presentation.io.ConsoleIO
+import java.util.*
+
+class FakeConsoleIO(
+    private val inputs: Queue<String>,
+    val outputs: MutableList<String> = mutableListOf()
+) : ConsoleIO {
+    override fun read(): String? = inputs.poll()
+    override fun write(message: String) {
+        outputs.add(message)
+    }
+}
