f6ee731227b20fb4ac5619541185dfd2a193baf4
alaafawzy <alaaf9443@gmail.com>
2025-04-24 23:30:45 +0200

    add test to GymHelperUI class

--- src/test/kotlin/presentation/GymHelperUITest.kt
+++ src/test/kotlin/presentation/GymHelperUITest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test
 
 class GymHelperUITest {
 
+
     private lateinit var getGymMealsUseCase: GetGymMealsUseCase
     private lateinit var consoleIO: ConsoleIO
     private lateinit var gymHelperUI: GymHelperUI
