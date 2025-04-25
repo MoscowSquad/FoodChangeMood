c80295b576f56268219d19e481fe7346cf79cabc
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-25 07:46:38 +0300

    add SearchMealByCountryUITest to cover all code

--- src/test/kotlin/presentation/SearchMealByCountryUITest.kt
+++ src/test/kotlin/presentation/SearchMealByCountryUITest.kt
@@ -23,12 +23,12 @@ class SearchMealByCountryUITest {
     }
 
     @Test
-    fun `should return meals matching with country`() {
+    fun `should return more than one meals matching with country`() {
         // Given
         val returnMeals = listOf(
             createMeal(name = "B-laban Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt"),
-            createMeal(name = "B-laban Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt"),
-            createMeal(name = "B-laban Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt")
+            createMeal(name = "Koshary Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt"),
+            createMeal(name = "qushtuta Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt")
         )
 
         every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns returnMeals
@@ -42,9 +42,93 @@ class SearchMealByCountryUITest {
         // Then
         assertThat(consoleIO.outputs).contains("Found 3 meals related to Egypt:")
         assertThat(consoleIO.outputs).contains("1. B-laban Egypt Meal")
-        assertThat(consoleIO.outputs).contains("2. B-laban Egypt Meal")
-        assertThat(consoleIO.outputs).contains("3. B-laban Egypt Meal")
+        assertThat(consoleIO.outputs).contains("2. Koshary Egypt Meal")
+        assertThat(consoleIO.outputs).contains("3. qushtuta Egypt Meal")
     }
 
+    @Test
+    fun `should return one meal matching with country`() {
+        // Given
+        val returnMeals = listOf(
+            createMeal(name = "Koshary Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt")
+        )
+
+        every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns returnMeals
+        consoleIO.inputs.add("Egypt")
+
+        // When
+        searchMealByCountryUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("Found 1 meals related to Egypt:")
+        assertThat(consoleIO.outputs).contains("1. Koshary Egypt Meal")
+    }
+
+    @Test
+    fun `should return one meal matching with country in name`() {
+        // Given
+        val returnMeals = listOf(
+            createMeal(name = "Koshary Egypt Meal", tags = listOf("iraq"), description = "This meal from iraq")
+        )
+
+        every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns returnMeals
+        consoleIO.inputs.add("Egypt")
+
+        // When
+        searchMealByCountryUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("Found 1 meals related to Egypt:")
+        assertThat(consoleIO.outputs).contains("1. Koshary Egypt Meal")
+    }
+
+    @Test
+    fun `should return one meal matching with country in tag`() {
+        // Given
+        val returnMeals = listOf(
+            createMeal(name = "Koshary Meal", tags = listOf("Egypt"), description = "This meal from iraq")
+        )
+
+        every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns returnMeals
+        consoleIO.inputs.add("Egypt")
+
+        // When
+        searchMealByCountryUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("Found 1 meals related to Egypt:")
+        assertThat(consoleIO.outputs).contains("1. Koshary Meal")
+    }
+
+    @Test
+    fun `should return one meal matching with country in description`() {
+        // Given
+        val returnMeals = listOf(
+            createMeal(name = "Koshary Meal", tags = listOf("iraq"), description = "This meal from Egypt")
+        )
+
+        every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns returnMeals
+        consoleIO.inputs.add("Egypt")
+
+        // When
+        searchMealByCountryUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("Found 1 meals related to Egypt:")
+        assertThat(consoleIO.outputs).contains("1. Koshary Meal")
+    }
+
+    @Test
+    fun `should return no meal found matching with country`() {
+        // Given
+        every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns emptyList()
+        consoleIO.inputs.add("Egypt")
+
+        // When
+        searchMealByCountryUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("No meals found for Egypt")
+    }
 
 }
