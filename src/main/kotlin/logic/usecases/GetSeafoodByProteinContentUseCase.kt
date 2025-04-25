02b37e4616f8e5e77f1dfc12e6a804871b552d7a
= <=>
2025-04-25 22:08:43 +0300

    guess game feature test cases

--- src/main/kotlin/logic/usecases/GetSeafoodByProteinContentUseCase.kt
+++ src/main/kotlin/logic/usecases/GetSeafoodByProteinContentUseCase.kt
@@ -1,12 +1,15 @@
 package org.example.logic.usecases
 
 import org.example.logic.repository.MealRepository
+import org.example.model.Exceptions.NoMealsFoundException
 import org.example.model.Meal
 
 class GetSeafoodByProteinContentUseCase(private val repository: MealRepository) {
     fun getSeafoodMealsByProteinContent(): List<Meal> {
         return repository.getAllMeals()
             .filter(::bySeafood)
+            .filter { it.nutrition?.protein != null }
+            .ifEmpty { throw NoMealsFoundException() }
             .sortedByDescending { it.nutrition?.protein }
     }
 
@@ -15,4 +18,3 @@ class GetSeafoodByProteinContentUseCase(private val repository: MealRepository) 
                 || meal.description?.contains("seafood") ?: false
     }
 }
-
