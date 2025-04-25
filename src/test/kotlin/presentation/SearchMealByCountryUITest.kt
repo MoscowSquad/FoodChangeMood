486f222d279ed4ecf0883b0148cde540d91c04cd
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-25 07:29:32 +0300

    add SearchMealByCountryUITest test case

--- src/test/kotlin/presentation/SearchMealByCountryUITest.kt
+++ src/test/kotlin/presentation/SearchMealByCountryUITest.kt
@@ -1,10 +1,14 @@
 package presentation
 
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
+import logic.usecases.createMeal
 import org.example.logic.usecases.SearchMealByCountryUseCase
 import org.example.presentation.SearchMealByCountryUI
 import org.junit.jupiter.api.BeforeEach
 import java.util.*
+import kotlin.test.Test
 
 class SearchMealByCountryUITest {
     private lateinit var searchMealByCountryUseCase: SearchMealByCountryUseCase
@@ -16,6 +20,30 @@ class SearchMealByCountryUITest {
         searchMealByCountryUseCase = mockk()
         consoleIO = FakeConsoleIO(LinkedList())
         searchMealByCountryUI = SearchMealByCountryUI(searchMealByCountryUseCase, consoleIO)
+    }
+
+    @Test
+    fun `should return meals matching with country`() {
+        // Given
+        val returnMeals = listOf(
+            createMeal(name = "B-laban Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt"),
+            createMeal(name = "B-laban Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt"),
+            createMeal(name = "B-laban Egypt Meal", tags = listOf("Egypt"), description = "This meal from Egypt")
+        )
+
+        every { searchMealByCountryUseCase.searchMealsByCountry("Egypt") } returns returnMeals
+
+
+        consoleIO.inputs.add("Egypt")
+
+        // When
+        searchMealByCountryUI.invoke()
+
+        // Then
+        assertThat(consoleIO.outputs).contains("Found 3 meals related to Egypt:")
+        assertThat(consoleIO.outputs).contains("1. B-laban Egypt Meal")
+        assertThat(consoleIO.outputs).contains("2. B-laban Egypt Meal")
+        assertThat(consoleIO.outputs).contains("3. B-laban Egypt Meal")
     }
 
 
