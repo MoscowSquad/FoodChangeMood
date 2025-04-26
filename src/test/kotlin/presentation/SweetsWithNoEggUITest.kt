f18180dc079b9b4900360d84af91858e907f0381
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-04-26 19:46:23 +0300

    test: improve tests SweetsWithNoEggUI

--- src/test/kotlin/presentation/SweetsWithNoEggUITest.kt
+++ src/test/kotlin/presentation/SweetsWithNoEggUITest.kt
@@ -2,46 +2,45 @@ package presentation
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verifySequence
 import logic.usecases.createMeal
 import org.example.logic.usecases.SweetsWithNoEggUseCase
 import org.example.model.Exceptions
 import org.example.presentation.SweetsWithNoEggUI
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.example.presentation.io.ConsoleIO
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.*
 
 class SweetsWithNoEggUITest {
 
     private lateinit var sweetsWithNoEggUseCase: SweetsWithNoEggUseCase
-    private lateinit var consoleIO: FakeConsoleIO
+    private lateinit var consoleIO: ConsoleIO
     private lateinit var sweetsWithNoEggUI: SweetsWithNoEggUI
 
     @BeforeEach
     fun setUp() {
         sweetsWithNoEggUseCase = mockk(relaxed = true)
-        consoleIO = FakeConsoleIO(LinkedList())
+        consoleIO = mockk(relaxed = true)
         sweetsWithNoEggUI = SweetsWithNoEggUI(sweetsWithNoEggUseCase, consoleIO)
     }
 
     @Test
     fun `should show a random sweet with no egg meal when success`() {
         // Given
-        every { sweetsWithNoEggUseCase() } returns createMeal(
+        val meal = createMeal(
             ingredients = listOf(),
             description = "sweet"
         )
+        every { sweetsWithNoEggUseCase() } returns meal
 
         // When
         sweetsWithNoEggUI()
 
         // Then
-        assertEquals(
-            listOf(
-                "🍬--- Sweets Without Eggs ---🍬",
-                "✨ Recommended Sweet:",
-            ), consoleIO.outputs.toList()
-        )
+        verifySequence {
+            consoleIO.write("🍬--- Sweets Without Eggs ---🍬")
+            consoleIO.write("✨ Recommended Sweet:")
+        }
     }
 
     @Test
@@ -54,11 +53,9 @@ class SweetsWithNoEggUITest {
         sweetsWithNoEggUI()
 
         // Then
-        assertEquals(
-            listOf(
-                "🍬--- Sweets Without Eggs ---🍬",
-                "No sweets found without eggs."
-            ), consoleIO.outputs.toList()
-        )
+        verifySequence {
+            consoleIO.write("🍬--- Sweets Without Eggs ---🍬")
+            consoleIO.write("No sweets found without eggs.")
+        }
     }
 }
